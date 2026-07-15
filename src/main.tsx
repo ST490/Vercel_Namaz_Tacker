f03b55c25eaf82ebba66221f4801d1417984f594
ST490 <ST490@users.noreply.github.com>
2026-07-15 11:43:27 +0530

    Revert "fix: dark bg for Quran widget, fix import paths"
    This reverts commit 3bb71e1f0b01577c3c53ee3b6a5705a86a10f548.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App'
+import App from '@/App.jsx'
 import '@/index.css'
 import { seedIfNeeded } from '@/lib/seedData';
 import { initStorage } from '@/lib/storage';
